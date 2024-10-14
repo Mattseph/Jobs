ded023c0e3737946211d524b164aac91ab357fdc
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-10-14 21:06:52 +0800

    Add: JobListings.vue File

---
+++ src/components/JobListings.vue
@@ -0,0 +1,40 @@
+<script setup>
+import JobListing from "@/components/JobListing.vue";
+import jobListingsData from "@/jobs.json";
+import { ref, defineProps } from "vue";
+
+defineProps({
+  limit: Number,
+  showButton: {
+    type: Boolean,
+    default: false,
+  }
+});
+const jobs = ref(jobListingsData);
+</script>
+
+<template>
+  <section class="bg-blue-50 px-4 py-10 border-indigo-800">
+    <div class="container-xl lg:container m-auto border-indigo-800">
+      <h2 class="text-bold text-3xl font-green-500 mb-6 text-center">
+        Job Listings
+      </h2>
+
+      <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+        <JobListing
+          v-for="job in jobs.slice(0, limit || jobs.length)"
+          :key="job.id"
+          :job="job"
+        />
+      </div>
+    </div>
+  </section>
+
+  <section v-if="showButton" class="m-auto max-w-lg my-10 px-6">
+    <a
+      href="/jobs"
+      class="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+      >View All Jobs</a
+    >
+  </section>
+</template>
