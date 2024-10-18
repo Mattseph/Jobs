3f0ef6ac96ceb30d9c83496ef05eea56a321c7a3
Mattseph <105849780+Mattseph@users.noreply.github.com>
2024-10-18 13:58:37 +0800

    Edit: Add Server Proxy

--- vite.config.js
+++ vite.config.js
@@ -8,6 +8,17 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+        rewrite: (path)=> path.replace(/^\/api/, ''), 
+      }
+    }
+  },
+
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
